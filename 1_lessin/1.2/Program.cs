2c8b7cd97b57702f35cc0cc9c8a25d53edb8ba6b
Nikita <nikitaraserg@outlook.com>
2022-08-08 17:20:19 +0300

    Creat project C# 1.2 and gitignore

---
+++ 1_lessin/1.2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число a");
+string number_a = Console.ReadLine();
+Console.WriteLine("Введите число b");
+string number_b = Console.ReadLine();
