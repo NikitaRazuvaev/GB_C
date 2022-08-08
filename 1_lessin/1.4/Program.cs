f9237f33794afdd7f2061505c136cb1e3e0126a0
Nikita <nikitaraserg@outlook.com>
2022-08-08 20:07:29 +0300

    Completed task 4 (1.4)

--- 1_lessin/1.4/Program.cs
+++ 1_lessin/1.4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число 1");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA < numberB){ Console.WriteLine(numberB.ToString());}
+else{ if (numberB < numberC){ Console.WriteLine(numberC.ToString());}
+if (numberC < numberA){ Console.WriteLine(numberA.ToString());}}
